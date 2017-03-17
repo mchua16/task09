357cd55afc347d07686b01d0f8da42dbe2c21602
mchua16 <MCHUA@vols.utk.edu>
2017-03-17 12:23:47 -0400

    Delete takemean.R

--- task_09/takemean/R/takemean.R
+++
@@ -1,7 +0,0 @@
-# takemean Function
-#
-# This function lets users take the mean of a numeric vector.
-
-takemean <- function(x) { #creates a function assigned as takemean
- mean(x) #takes the mean of x
-}
