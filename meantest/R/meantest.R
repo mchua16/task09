ac02258da60ba93dd7d7f852ecb064299e6256e0
mchua16 <mchua@vols.utk.edu>
2017-03-17 12:12:17 -0400

    second commit

---
+++ meantest/R/meantest.R
@@ -0,0 +1,12 @@
+#' meantest Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords meantest 
+#' @export
+#' @examples
+#' meantest()
+
+meantest <- function(x) { 
+  mean(x)
+}
